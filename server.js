a1d0d36d7ef0caf87e49535300945ec5f47cd54b
innocent <ekweozorinnocent@gmail.com>
2024-03-06 01:15:21 +0100

    get random quote

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const generateAQuote = require("./quoteGenerator");
 
 const app = express();
+
+app.get("/api/quote", (req, res) => {
+    const quote = generateAQuote()
+    res.json({quote})
+})
 
 // Start the server
 app.listen(3000, () => {
